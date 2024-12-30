8195fc5bc73b1804118421f686f57d8c0836e84b
Marcus Anderson <manderson332@gatech.edu>
2024-12-29 23:40:05 -0500

    added invlaid input functionality

--- src/app/profile/contact/contact.component.ts
+++ src/app/profile/contact/contact.component.ts
@@ -1,29 +1,70 @@
 import { Component, OnInit } from '@angular/core';
 import { ContactFormData } from '../profile.interface';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-contact',
   templateUrl: './contact.component.html',
   styleUrls: ['./contact.component.scss']
 })
-export class ContactComponent implements OnInit {
- 
+export class ContactComponent {
+
   contactFormData: ContactFormData = {
-    name: '',
-    subject: '',
-    email: '',
-    message: ''
+    name: { input: '', invalid: false },
+    subject: { input: '', invalid: false },
+    email: { input: '', invalid: false },
+    message: { input: '', invalid: false }
   };
-  invalid = false;
 
-  constructor(){}
+  constructor(
+    private http: HttpClient
+  ) { }
 
-  ngOnInit() {}
+  onSubmit(contactForm: any): void {
+    const googleSheetsScriptURL = 'https://script.google.com/macros/s/AKfycbz-XJd51Q3oN-jKiGINIYYVQbuqH2bAhTrUtSBJ_HRKDUanZ-MrMo_n7T-g-syjKpo/exec';
+    const isValid = this.checkFormData(this.contactFormData, contactForm);
+    if (isValid) {
+      const reformattedFormData = this.reformatFormData(this.contactFormData);
+      this.http.post(googleSheetsScriptURL, reformattedFormData, { headers: { 'Content-Type': 'text/plain' } }
+      ).subscribe({
+        next: () => {
+          console.log('Form data logged to Google Sheets.');
+        },
+        error: (err) => {
+          console.error('Error logging data:', err);
+        }
+      });
+      this.clearForm(contactForm);
+    }
+  }
 
-  onSubmit() {
-    console.log('Form submitted:', this.contactFormData);
-    this.invalid = true;
-}
+  private checkFormData(submittedForm: ContactFormData, contactForm: any): boolean {
+    const formData = contactForm.form;
+    const formDataControls = formData.controls;
 
+    submittedForm.name.invalid = formDataControls.name.errors != null;
+    submittedForm.subject.invalid = formDataControls.subject.errors != null;
+    submittedForm.email.invalid = formDataControls.email.errors != null;
+    submittedForm.message.invalid = formDataControls.message.errors != null;
+    return formData.status === 'VALID';
+  }
 
+  private clearForm(form: any): void {
+    this.contactFormData = {
+      name: { input: '', invalid: false },
+      subject: { input: '', invalid: false },
+      email: { input: '', invalid: false },
+      message: { input: '', invalid: false }
+    }
+    form.resetForm();
+  }
+
+  private reformatFormData(formData: ContactFormData): any {
+    return {
+      name: formData.name.input,
+      subject: formData.subject.input,
+      email: formData.email.input,
+      message: formData.message.input
+    };
+  }
 }
--- src/app/profile/profile.interface.ts
+++ src/app/profile/profile.interface.ts
@@ -1,6 +1,12 @@
+
 export interface ContactFormData {
-    name: string;
-    subject: string;
-    email: string;
-    message:string;
+    name: FormInput;
+    subject: FormInput;
+    email: FormInput;
+    message: FormInput;
+}
+
+export interface FormInput {
+    input: string;
+    invalid: boolean;
 }
